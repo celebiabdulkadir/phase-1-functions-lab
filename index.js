cd193cdcc44285a554f77d6d9f76954133d495c7
Abdulkadir Celebi <abdulkadircelebi.ac@gmail.com>
2023-05-11 09:59:39 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+// Assumes that the HQ is on 42nd street
+function distanceFromHqInBlocks(blockNumber) {
+  return Math.abs(blockNumber - 42);
+}
+
+function distanceFromHqInFeet(blockNumber) {
+  const blocks = distanceFromHqInBlocks(blockNumber);
+  return blocks * 264; // as one block is approximately 264 feet
+}
+
+function distanceTravelledInFeet(start, destination) {
+  return Math.abs(start - destination) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+
+  if (distance <= 400) {
+    return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return (distance - 400) * 0.02;
+  } else if (distance > 2000 && distance <= 2500) {
+    return 25;
+  } else {
+    return "cannot travel that far";
+  }
+}
